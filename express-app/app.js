c6e242c54fa1cdbfb0bc8c0682db0e05f2518f95
Yuzunoha <studysteadystandby@gmail.com>
2021-11-11 01:33:39 +0900

    pipeは勉強しないとね

--- express-app/app.js
+++ express-app/app.js
@@ -7,15 +7,9 @@ const p = console.log;
 var server = http.createServer((req, res) => {
   var urlParts = url.parse(req.url);
   var path = __dirname + '/' + urlParts.pathname;
-  p('path', path);
   var stream = fs.createReadStream(path);
 
-  stream.on('data', (data) => {
-    res.write(data);
-  });
-  stream.on('end', (data) => {
-    res.end();
-  });
+  stream.pipe(res);
 });
 
 server.listen(80);
