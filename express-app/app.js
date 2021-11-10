50730c22b88aa163870a4de81d120ab715718416
Yuzunoha <studysteadystandby@gmail.com>
2021-11-10 21:02:54 +0900

    ejsはまだ使わない

--- express-app/app.js
+++ express-app/app.js
@@ -1,11 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 
 const p = console.log;
 
 var server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('こんにちは、世界!');
-  res.end();
+  fs.readFile('./temp.html', 'utf-8', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
 });
 
 server.listen(80);
