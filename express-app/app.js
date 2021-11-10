451c81bbc733eda7ff0d18bb5184a3d4febeb2d7
Yuzunoha <studysteadystandby@gmail.com>
2021-11-10 21:29:33 +0900

    画面遷移

--- express-app/app.js
+++ express-app/app.js
@@ -4,7 +4,23 @@ var fs = require('fs');
 const p = console.log;
 
 var server = http.createServer((req, res) => {
-  fs.readFile('./temp.html', 'utf-8', (err, data) => {
+  var target = '';
+
+  switch (req.url) {
+    case '/':
+    case '/index':
+      target = './index.html';
+      break;
+    case '/next':
+      target = './next.html';
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('bad request');
+      return;
+  }
+
+  fs.readFile(target, 'utf-8', (err, data) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     res.end();
