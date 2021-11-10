a8a2cebc4ac965bf06cce968754b3a09ac00ed43
Yuzunoha <studysteadystandby@gmail.com>
2021-11-10 20:58:59 +0900

    サーバが起動した

--- express-app/app.js
+++ express-app/app.js
@@ -1,31 +1,12 @@
 var http = require('http');
-var morgan = require('morgan');
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
 
-var app = express();
+const p = console.log;
 
-app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'ejs');
-
-app.use(morgan('combined'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/static', express.static(path.join(__dirname, 'static')));
-
-app.get('/', (req, res) => {
-  return res.render('index', { title: 'こんにちは世界!ejs' });
-});
-app.get('/form', (req, res) => {
-  return res.render('form');
-});
-app.post('/form', (req, res) => {
-  const obj = {
-    username: req.body.username,
-    message: req.body.message,
-  };
-  return res.render('result', obj);
+var server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('こんにちは、世界!');
+  res.end();
 });
 
-var server = http.createServer(app);
 server.listen(80);
+p('サーバを起動しました。');
