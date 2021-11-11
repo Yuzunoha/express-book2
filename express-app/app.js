9811399c273e0133b4126618a13707581d8bd411
Yuzunoha <studysteadystandby@gmail.com>
2021-11-11 15:29:38 +0900

    エクスプレスは記述が少なく済むね

--- express-app/app.js
+++ express-app/app.js
@@ -1,23 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-var ejs = require('ejs');
+var express = require('express');
+var app = express();
 
-var main = fs.readFileSync('./main.ejs', 'utf-8');
-var sub = fs.readFileSync('./sub.ejs', 'utf-8');
 const p = console.log;
 
-var server = http.createServer((req, res) => {
-  const contents = ejs.render(sub, {
-    data: '<p>hogehoge</p>',
-  });
-  var data = ejs.render(main, {
-    data: contents,
-  });
-
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(data);
-  res.end();
+app.get('/', (req, res) => {
+  res.send('Hello Expressあああ');
 });
 
-server.listen(80);
-p('サーバを起動しました。');
+app.listen(80, () => {
+  p('サーバを起動しました。');
+});
