dc957dcc0c1dc28863813d489018f4a87bbe4e78
Yuzunoha <studysteadystandby@gmail.com>
2021-11-11 15:38:29 +0900

    ejsファイルはviewsに入れないといけないらしい

--- express-app/app.js
+++ express-app/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var app = express();
+var ejs = require('ejs');
 
+var app = express();
 const p = console.log;
 
+app.engine('ejs', ejs.renderFile);
+
 app.get('/', (req, res) => {
-  res.send('Hello Expressあああ');
+  const contents = '<p>hoge</p>';
+  res.render('temp.ejs', {
+    contents,
+  });
 });
 
 app.listen(80, () => {
