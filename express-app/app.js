f58dc08311c2a2375439030ee0d0afe539370321
Yuzunoha <studysteadystandby@gmail.com>
2021-11-11 16:01:09 +0900

    引数で分割代入

--- express-app/app.js
+++ express-app/app.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var ejs = require('ejs');
+var bodyParser = require('body-parser');
 
 var app = express();
 const p = console.log;
 
 app.engine('ejs', ejs.renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const disp = (msg, { name, age }) => {
+  p(msg);
+  p(name);
+  p(age);
+};
 
 app.get('/', (req, res) => {
-  res.render('temp.ejs', {
-    title: 'タイトルだよー',
-    contents: '<p>hoge</p>',
-  });
+  disp('GET', req.query);
+  res.render('temp.ejs', {});
+});
+
+app.post('/', (req, res) => {
+  disp('POST', req.body);
+  res.render('temp.ejs', {});
 });
 
 app.listen(80, () => {
